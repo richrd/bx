2cddfc7d5a2f72f77a041b5ede32ab64da5cc41f
Richard Lewis <richrd.lewis@gmail.com>
2014-09-20 14:48:17 +0300

    implemented adding and removing channels

---
+++ modules/addchan.py
@@ -0,0 +1,23 @@
+from mod_base import*
+
+class AddChan(Command):
+    """Change the nick of the bot."""
+    def run(self, win, user, data, caller=None):
+        args = Args(data)
+        if args.Empty():
+            win.Send("specify channel to add to autojoin list")
+            return False
+        if self.bot.IsChannelName(args[0]):
+            if self.bot.config.AddChannel(args[0]):
+                win.Send("channel added")
+                return True
+            win.Send("channel already exists")
+        else:
+            win.Send("invalid channel name")
+
+module = {
+    "class": AddChan,
+    "type": MOD_COMMAND,
+    "level": 5,
+    "zone":IRC_ZONE_BOTH
+}
---
+++ modules/delchan.py
@@ -0,0 +1,23 @@
+from mod_base import*
+
+class DelChan(Command):
+    """Change the nick of the bot."""
+    def run(self, win, user, data, caller=None):
+        args = Args(data)
+        if args.Empty():
+            win.Send("specify channel to delete from autojoin list")
+            return False
+        if self.bot.IsChannelName(args[0]):
+            if self.bot.config.RemoveChannel(args[0]):
+                win.Send("channel removed")
+                return True
+            win.Send("channel doesn't exists")
+        else:
+            win.Send("invalid channel name")
+
+module = {
+    "class": DelChan,
+    "type": MOD_COMMAND,
+    "level": 5,
+    "zone":IRC_ZONE_BOTH
+}
