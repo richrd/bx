c84dfb28a357eafdaeff3611a08ee958de8a2d1f
Richard Lewis <richrd.lewis@gmail.com>
2014-10-09 20:34:01 +0300

    Branched initial logger

---
+++ logger.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Logging capabilities IRC Bot.
+"""
+
+class colors:
+    BLACK = '\033[30m'
+    WHITE = '\033[37m'
+    GREEN = '\033[32m'
+    MAGENTA = '\033[95m'
+    BLUE = '\033[94m'
+    DARKBLUE = '\033[34m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+class Logger:
+    def __init__(self, bot):
+        self.bot = bot
+        self.logs = []
+        self.domain_colors = {
+            "irc": colors.DARKBLUE,
+            "bot": colors.WHITE,
+            "cmd": colors.MAGENTA,
+        }
+
+    def Log(self, domain, s, color=None):
+        if not color and domain in self.domain_colors.keys():
+            color = self.domain_colors[domain]
+        self.logs.append([domain, s, color])
+        line = time_stamp_short()+" "+s
+        if color:
+            line = color + line + colors.ENDC
+        print line
+
+    def Warning(self, domain, s, color=None):
+        self.Log(domain, s, colors.FAIL)
