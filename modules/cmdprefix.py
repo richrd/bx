abf61178a639900ad1725a8fa3ad31ec1c40247d
Richard Lewis <richrd.lewis@gmail.com>
2014-10-16 22:29:14 +0300

    Added checking prefix to cmdprefix.

--- modules/cmdprefix.py
+++ modules/cmdprefix.py
@@ -1,11 +1,12 @@
 from mod_base import *
 
 class CmdPrefix(Command):
-    """Set the command prefix that the bot will respond to."""
+    """Check or set the command prefix that the bot will respond to."""
     def run(self, win, user, data, caller=None):
         args = Args(data)
         if args.Empty():
-            win.Send("specify prefix to use for detecting commands")
+            cp = self.bot.config["cmd_prefix"]
+            win.Send("current command prefix is: " + cp)
             return False
         self.bot.config["cmd_prefix"] = args[0]
         win.Send("done")
