915a26bf919432f73a090168a365422d7c967a84
Richard Lewis <richrd.lewis@gmail.com>
2014-11-27 00:49:06 +0200

    Fixed wather module.

--- modules/weather.py
+++ modules/weather.py
@@ -4,13 +4,11 @@ import time, json
 
 class Weather(Command):
     """Display the current weather conditions. Usage: weather [location]"""
-
     def init(self):
-        self.api_key = "5d4f25ba8d50f6bd0a30e8f32560a669"
-        self.api_url = "http://api.openweathermap.org/data/2.5/weather?q=[[QUERY]]&units=metric&APPID="+self.api_key
-        self.default_query = "Helsinki,fi"
-        self.cache_max_age = 60*2   # cache results for 2 minutes
-
+        self.storage.Load()
+        self.api_url_base = "http://api.openweathermap.org/data/2.5/weather?q=[[QUERY]]&units=metric&APPID="
+        self.default_query = "helsinki"
+        self.cache_max_age = 60*5   # cache results for 5 minutes
         # Queries are cached with city as dict key
         # Cache data is stored as (timestamp, data)
         self.cache = {}
@@ -26,9 +24,11 @@ class Weather(Command):
 
     def get_weather_data(self, query):
         # Must use this encoding workaround (fixed in Python 3)
-        query = urllib.quote(query.lower().encode("utf8"))
-        url = self.api_url.replace("[[QUERY]]", query)
-        if query in self.cache.keys() and time.time()-self.cache[query][0] > self.cache_max_age:
+        query = query.encode("utf8")
+        query = urllib.quote(query.lower().strip())
+        url = self.api_url_base + self.storage["api_key"]
+        url = self.api_url_base.replace("[[QUERY]]", query)
+        if query in self.cache.keys() and time.time()-self.cache[query][0] < self.cache_max_age:
             return self.cache[query][1]
         else:
             data = self.get_url_data(url)
@@ -44,12 +44,10 @@ class Weather(Command):
         query = self.default_query
         if not args.Empty():
             query = args.Range(0)
-
         wdata = self.get_weather_data(query)
         if not wdata:
             win.Send("Sorry, weather data can't be retrieved.")
             return False
-
         code = wdata["cod"]
         if code != 200:
             message = ""
@@ -58,20 +56,21 @@ class Weather(Command):
             win.Send("Sorry, weather unavailable." + message)
             return False
 
+        stamp = wdata["dt"]
         weather = wdata["weather"][0]
-        temp = str(wdata["main"]["temp"]) # Convert from Kelvin to Celcius
+        temp = str(wdata["main"]["temp"])
         humidity = str(wdata["main"]["humidity"])
         windspeed = str(wdata["wind"]["speed"])
         clouds = str(wdata["clouds"]["all"])
         place = wdata["name"]
-
         condition = weather["description"] + ", "
         output = condition + ", ".join([
                 temp + u"\u00B0C",
                 "wind speed " + windspeed + "m/s",
                 "cloudiness " + clouds + "%",
                 "humidity " + humidity + "%",
-                "@ " + place
+                "@ " + place,
+                # time.strftime("[%b %d %Y %H:%M:%S GMT]", time.gmtime(stamp)) # debug cache
             ])
         win.Send(output)
 
@@ -80,4 +79,7 @@ module = {
     "type": MOD_COMMAND,
     "level": 0,
     "zone":IRC_ZONE_BOTH,
+    "storage": {
+        "api_key": ""
+    }
 }
